5c067f3ebab47cf8490121ee4eacffb66fefd639
amitk2007 <amitk2007@gmail.com>
2016-11-05 11:47:00 +0200

    final?

---
+++ Assets/Main And End Game/CanvasSizeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasSizeScript : MonoBehaviour {
+
+	public Canvas canvas;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
